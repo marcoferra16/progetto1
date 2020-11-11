14623281c8cabf1f6ee28359629692557ba1c572
Marco Ferrario <ferrario.marco02@gmail.com>
2020-11-11 13:05:07 +0100

    def home news

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', homepage,name='homepage'),
     path('prima_app/', include('prima_app.urls')),
     path('seconda_app/', include('seconda_app.urls')),
+    path('news/', include('news.urls')),
 ]
