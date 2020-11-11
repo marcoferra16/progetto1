96677c447d92454b42cb74a79760354b50e388d0
Marco Ferrario <ferrario.marco02@gmail.com>
2020-11-11 13:30:39 +0100

    update class home

--- news/views.py
+++ news/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from  django.http import HttpResponse
+from .models import Articolo, Giornalista
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Homepage news!</h1>')
+    a = ''
+    g = ''
+    
+    for art in Articolo.objects.all():
+        a += (art.titolo + '<br>')
+
+    for gio in Giornalista.objects.all():
+        g += (gio.nome + '<br>')
+    
+    response = 'articolo:<br>' + a + '<br>Giornalisra<br>' + g
+
+    return HttpResponse('<h1>' + response + '</h1>')
