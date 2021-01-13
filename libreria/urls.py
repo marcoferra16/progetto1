1dba12384cad7683fb92a9ab5f2aa98198f0bd8f
marco_ferra <ferrario.marco02@gmail.com>
2021-01-13 13:47:18 +0100

    fix libro.html and autore.html

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import LibroList_fm, AutoreDetail_fm
+app_name='libreria'
 urlpatterns = [
     path('',LibroList_fm.as_view(), name="lista_libri"),
     path('autore/<int:pk>/',AutoreDetail_fm.as_view(), name="profilo_autore")    
--- libreria/views.py
+++ libreria/views.py
@@ -5,10 +5,12 @@ from .models import AUTORE_fm, LIBRO_fm
 # Create your views here.
 
 class AutoreDetail_fm(DetailView):
+    context_object_name="autore"
     model = AUTORE_fm
     template_name = "autore.html"
 
 class LibroList_fm(ListView):
+    context_object_name="libri"
     model = LIBRO_fm
     template_name = "libro.html"
 
