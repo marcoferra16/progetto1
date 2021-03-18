6c6fe7eef9557e5403d7e9a5ce178059b21d3b45
marco_ferra <ferrario.marco02@gmail.com>
2021-03-18 20:15:52 +0100

    L26

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Contatto
+from django.core.exceptions import ValidationError
 
 class FormContatto(forms.ModelForm):
     class Meta:
         model = Contatto
         fields = "__all__"
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'cognome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'contenuto': forms.TextInput(attrs={'placeholder': 'Area testuale inserisci almeno 20 caratteri', 'class': 'form-control'})
+        }
+
+    def clean_contenuto(self): #nome funzione: clean_nomecampodavalidare
+        dati = self.cleaned_data["contenuto"]
+        if "parola" in dati: #parola non è ammesso
+            raise ValidationError("Il contenuto inserito viola le norme del sito!")
+        if len(dati)<20:
+            raise ValidationError("Il contenuto inserito è troppo breve")
+        return dati
